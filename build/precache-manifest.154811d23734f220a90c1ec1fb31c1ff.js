7ae4ed39512e4ce7f9a4b441fa7f307ac5d080dd
CharlieMansion <mansion.ch@gmail.com>
2020-01-09 00:21:41 +0100

    adding button to play video for mobile 16

--- build/precache-manifest.154811d23734f220a90c1ec1fb31c1ff.js
+++ build/precache-manifest.154811d23734f220a90c1ec1fb31c1ff.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8244b8a98d722fb09f1a3ea3741052cc",
+    "revision": "72199bdf73b943dd463f097fc57e7e64",
     "url": "/hiremeascontentcreator/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hiremeascontentcreator/static/css/2.1af7a3c5.chunk.css"
   },
   {
-    "revision": "8ffcfee7bae1a11d42e3",
-    "url": "/hiremeascontentcreator/static/css/main.b7d24c7c.chunk.css"
+    "revision": "cd390b4c9c2ed41dc619",
+    "url": "/hiremeascontentcreator/static/css/main.07ea2ee9.chunk.css"
   },
   {
     "revision": "e7ddd2f9267106b5e761",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hiremeascontentcreator/static/js/2.f2790ad1.chunk.js.LICENSE"
   },
   {
-    "revision": "8ffcfee7bae1a11d42e3",
+    "revision": "cd390b4c9c2ed41dc619",
     "url": "/hiremeascontentcreator/static/js/main.d4960c79.chunk.js"
   },
   {
