2899d4bb148b1c05baa520fe0d8806ca8e0b5742
CharlieMansion <mansion.ch@gmail.com>
2020-01-10 23:47:56 +0100

    adding cv page

--- build/precache-manifest.99e62ff894fe7c91c3049e0b6e3bc711.js
+++ build/precache-manifest.99e62ff894fe7c91c3049e0b6e3bc711.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "916d8fd7f2de84684b460b602dd27e27",
+    "revision": "399422a5d4e22223418dede78b2f2e31",
     "url": "/hiremeascontentcreator/index.html"
   },
   {
-    "revision": "7984af8e83e04e68aac3",
+    "revision": "0355e4ddaba7df2ea1b4",
     "url": "/hiremeascontentcreator/static/css/2.1af7a3c5.chunk.css"
   },
   {
-    "revision": "54f3e2f89a1c12b856e1",
-    "url": "/hiremeascontentcreator/static/css/main.14c88e02.chunk.css"
+    "revision": "22a065f33660f6b29faa",
+    "url": "/hiremeascontentcreator/static/css/main.148a7a8a.chunk.css"
   },
   {
-    "revision": "7984af8e83e04e68aac3",
-    "url": "/hiremeascontentcreator/static/js/2.3fa928a5.chunk.js"
+    "revision": "0355e4ddaba7df2ea1b4",
+    "url": "/hiremeascontentcreator/static/js/2.10cfd684.chunk.js"
   },
   {
     "revision": "b16c193132695216b3273f2acb3f96c7",
-    "url": "/hiremeascontentcreator/static/js/2.3fa928a5.chunk.js.LICENSE"
+    "url": "/hiremeascontentcreator/static/js/2.10cfd684.chunk.js.LICENSE"
   },
   {
-    "revision": "54f3e2f89a1c12b856e1",
-    "url": "/hiremeascontentcreator/static/js/main.8b505723.chunk.js"
+    "revision": "22a065f33660f6b29faa",
+    "url": "/hiremeascontentcreator/static/js/main.59e69f3c.chunk.js"
   },
   {
     "revision": "3ef0868e9fb72f808757",
@@ -34,6 +34,18 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ea66bf8234bbe1ee203ced361c102a48",
     "url": "/hiremeascontentcreator/static/media/PIC3.ea66bf82.jpg"
+  },
+  {
+    "revision": "4d0639a7695d00170c28fa336505a8a0",
+    "url": "/hiremeascontentcreator/static/media/dribbble-icon-dark-gray.4d0639a7.png"
+  },
+  {
+    "revision": "e26d94c97c4939decdcfffeded34022f",
+    "url": "/hiremeascontentcreator/static/media/github-icon-dark-gray.e26d94c9.png"
+  },
+  {
+    "revision": "2a632834ce51b282c70da4c47a9e5423",
+    "url": "/hiremeascontentcreator/static/media/linkedin-icon-dark-gray.2a632834.png"
   },
   {
     "revision": "8659515094d85059439811bbedcbbc39",
